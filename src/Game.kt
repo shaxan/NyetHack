d13f7ac81af2972ccf86cb7240f1a99e981b3eda
SALAVAT <SALAWATB@GMAIL.COM>
2020-01-23 16:40:12 +0600

    Initial commit

---
+++ src/Game.kt
@@ -0,0 +1,35 @@
+fun main() {
+    val name = "Salawat"
+    var healthPoints = 100
+    val isBlessed = true
+    val isImmortal = false
+//    Ayra
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val karma = (Math.pow(Math.random(), (100 - healthPoints) / 100.0) *
+            20).toInt()
+    val auraColor = when (karma) {
+        in 0..5 -> "red"
+        in 6..10 -> "orange"
+        in 11..15 -> "purple"
+        in 16..20 -> "green"
+        else -> ""
+    }
+    // val auraColor = if (auraVisible) "GREEN" else "NONE"
+    //println("Aura: $auraColor " + "Blased: ${if (isBlessed) "Yes" else "NO"}")
+    // println("$name здорвье $healthPoints %")
+    val healthStatus = when (healthPoints) {
+        100 -> " в отличном состоянии!"
+        in 90..99 -> " имеет несколько царапин"
+        in 75..89 -> if (isBlessed) {
+            " имеет незначительные раны, но заживает довольно быстро!"
+        } else {
+            " имеет незначительные раны"
+        }
+        in 15..74 -> " выглядит довольно больной."
+        else -> "в ужасном состоянии!"
+    }
+    println("(HP: $healthPoints %)" + ("(Аура: $auraColor)") + " -->" + "\"$name" + "$healthStatus\"")
+
+
+}
+
